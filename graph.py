19fcb2f18a606f7293039fd6dc82190f9a151bdf
Josiah Tan <jtan7828@uni.sydney.edu.au>
2022-04-28 00:06:51 +1000

    changed the line thickness to make more visible

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 plots = {}
 plt.clf()
@@ -14,11 +15,48 @@ with open('monitor_log.txt', 'r') as myfile:
 
 for key, val in plots.items():
     val = zip(*val)
+    key = key.replace(':', '')
     print(key)
-    plt.plot(*val, label=key)
-    plt.title("Motor Trajectory")
-    plt.ylabel("y")
-    plt.xlabel("x")
-    plt.legend()
-    plt.show()
-    plt.savefig("output.png")
+    plt.plot(*val, label=key, linewidth=0.5)
+plt.title("Motor Trajectory")
+plt.ylabel("y")
+plt.xlabel("x")
+plt.legend()
+plt.show()
+plt.savefig("output.png")
+
+# plt.plot(range(0, 1, 100))
+# plt.show()
+# plt.savefig("output.png")
+
+
+##
+
+
+# importing the required module
+import matplotlib.pyplot as plt
+plt.clf()
+
+# x axis values
+x = [1,2,3]
+# corresponding y axis values
+y = [2,4,1]
+
+# plotting the points
+plt.plot(x, y)
+
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+
+# giving a title to my graph
+plt.title('My first graph!')
+ 
+ # function to show the plot
+plt.show()
+plt.savefig("output.png")
+
+
+##
+
