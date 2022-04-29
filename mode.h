4aacae8a948de9dae773bd9e5de0d150471e7a8b
Josiah Tan <jtan7828@uni.sydney.edu.au>
2022-04-29 21:08:45 +1000

    implemented pwm serial test, altered open loop test

--- mode.h
+++ mode.h
@@ -22,6 +22,7 @@ class Mode{
 		Controller controller;
 	public:
 		Mode(MODE _mode): _mode(_mode) {
+			/* constructor: initialization of all objects required for each mode */
 			if (_mode == PWM_TEST){
 				PWM pwm;
 			}
@@ -32,6 +33,16 @@ class Mode{
 				PWM pwm;
 				serial = SerialDriver();
 				sensor = Sensor(3, 4);
+			}
+			else if (_mode == OPEN_LOOP_TEST){
+				PWM pwm;
+				serial = SerialDriver();
+				sensor = Sensor(3, 4);
+				controller = Controller(0, 0, 0, true);
+			}
+			else if (_mode == PWM_SERIAL_TEST){
+				PWM pwm;
+				serial = SerialDriver();
 			}
 			else if (_mode == CLOSED_LOOP_TEST){
 				PWM pwm;
@@ -39,14 +50,9 @@ class Mode{
 				sensor = Sensor(3, 4);
 				controller = Controller(1, 1, 0.01, true);
 			}
-			else if (_mode == OPEN_LOOP_TEST){
-				PWM pwm;
-				serial = SerialDriver();
-				sensor = Sensor(3, 4);
-				controller = Controller(1, 0, 0, true);
-			}
 		}
 		void setup(){
+			/* setup: calling the setup function of all objects for each mode (if required) */
 			if (_mode == PWM_TEST){
 				pwm.setup();
 			}
@@ -62,6 +68,10 @@ class Mode{
 				pwm.setup();
 				serial.setup();
 				sensor.setup();
+			}
+			else if (_mode == PWM_SERIAL_TEST){
+				pwm.setup();
+				serial.setup();
 			}
 			else if (_mode == OPEN_LOOP_TEST){
 				pwm.setup();
@@ -70,50 +80,50 @@ class Mode{
 			}
 		}
 		void loop(){
+			/* function: code to run on a loop */
 			if (_mode == PWM_TEST){
+				// test pwm in both the backward and the forward directions
 				pwm.set(99);
 				delay(5000);
 				pwm.set(1);
 				delay(5000);
-				// PWM_Test::simple();
 			}
 			else if (_mode == SERIAL_TEST) {
+				// try different types of serial testing (see serial.h for more information on the api)
 				// serial.test();
 				// serial.testSend();
 				serial.testGUI();
 			}
 			else if (_mode == SPEED_READ_TEST){
+				// testing the direction and the speed of the motor (see sensor.h for more information on the api)
 				// should be anticlockwise
 				pwm.set(25);
 				// should be clockwise
 				// pwm.set(75);
-				// PWM_Test::simple();
 				sensor.test();
 			}
-			else if (_mode == CLOSED_LOOP_TEST){
+			else if (_mode == PWM_SERIAL_TEST){
+				// accepts values between [1, 99]
+				// assumes that pwm values are being sent, the motor should spin at 0 (the default)
+				int reference = serial.getReference();
+				pwm.set(reference);
+				Serial.println("reference: ");
+				Serial.println(reference);
+			}
+			else if (_mode == CLOSED_LOOP_TEST || _mode == OPEN_LOOP_TEST){
+				// accepts values between (-99, 99)
+				// implementation of PID control (note that open_loop_test has 0 for the PID constants)
 				int reference = serial.getReference();
 				int speed = sensor.getSpeed();
+				int input = controller.pid(speed, reference);
+				pwm.set(input);
+				// print values for graphing
 				Serial.print("reference: ");
 				Serial.println(reference);
 				Serial.print("speed: ");
 				Serial.println(speed);
-				int input = controller.pid(speed, reference);
-				// pwm.set(50);
 				Serial.print("pwm: ");
 				Serial.println(input);
-				pwm.set(input);
-				// delay(100);
-				// delay(1);
-				// delay(100);
-			}
-			else if (_mode == OPEN_LOOP_TEST){
-				// assumes that you are sending pwm values, so don't get confused when the initial value is 0, and that causes the motor to spin
-				int reference = serial.getReference();
-				// int speed = sensor.getSpeed();
-				// int input = controller.pid(speed, reference);
-				// pwm.set(50);
-				pwm.set(reference);
-				Serial.println(reference);
 			}
 		}
 };
