356388e893c5c6820acad3dc35e44eb25442b50c
Josiah Tan <jtan7828@uni.sydney.edu.au>
2022-04-29 21:06:28 +1000

    added a new state: pwm serial test
    pwm serial test basically just allows us to set the pwm using the serial
    open loop test should really be testing the open loop, rather than doing
    what pwm serial test is currently doing

--- project.ino
+++ project.ino
@@ -1,12 +1,13 @@
 
 #include "mode.h"
 
-// Mode mode(SERIAL_TEST);
-/* Mode mode(PWM_TEST); */
-/* Mode mode(SPEED_READ_TEST); */
-Mode mode(CLOSED_LOOP_TEST);
-// Mode mode(OPEN_LOOP_TEST);
-/* Mode mode(IDLE); */
+// Mode mode(SERIAL_TEST); // test if the serial is working
+// Mode mode(PWM_TEST); // test if the pwm output is working
+// Mode mode(SPEED_READ_TEST); // test if the speed reading (sensor) is working
+Mode mode(CLOSED_LOOP_TEST); // test if the closed loop system is working
+// Mode mode(OPEN_LOOP_TEST); // test if the open loop system is working
+// Mode mode(PWM_SERIAL_TEST); // test if the pwm combined with serial is working
+// Mode mode(IDLE); // set to idle (do nothing)
 void setup() {
 	mode.setup();
 }
